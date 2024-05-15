1ba36dcdd93a0a8123ece9e2efa38082393aac97
adrian <ferrufinoadrian70@gmail.com>
2024-05-15 13:37:38 -0400

    CMP-01 Fnished Pre Procceser

--- Preprocesador.py
+++ Preprocesador.py
@@ -2,9 +2,28 @@
 # %**% comentario varias lineas
 
 def preprocess(txt_filepath):
+    result = ""
+    adding = True
     with open(txt_filepath, "+r") as file:
         lines = file.readlines()
         for line in lines:
-            if line[0] == 
-        pass
-    pass
+            for i in range(len(line)):
+                if line[i] == "%":
+                    if i+1 != len(line):
+                        if line[i + 1] == "*":
+                            adding = False
+                        else:
+                            break
+                elif line[i] == "*" and not adding:
+                    if i+1 != len(line):
+                        if line[i + 1] == "%":
+                            adding = True
+
+                elif adding:
+                    if line[i] != " " or line[i] != "\t" or line[i] != "\n":
+                        result += line[i]
+
+            #if len(result) > 0 and result[-1] != "\n":
+            #    result += "\n"
+
+    return result.strip()
