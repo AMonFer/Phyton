86be8e40a1937893501ff468c3113d4923654cd0
adrian <ferrufinoadrian70@gmail.com>
2024-05-15 13:12:51 -0400

    CMP-01 Created proyect

---
+++ Preprocesador.py
@@ -0,0 +1,10 @@
+# % comentario una linea
+# %**% comentario varias lineas
+
+def preprocess(txt_filepath):
+    with open(txt_filepath, "+r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if line[0] == 
+        pass
+    pass
